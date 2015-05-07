fb9b46e03f3dd5d6efe33191f505e07737df4cbf
AmberGlasses <775837740@qq.com>
2015-05-07 18:03:42 +0800

    测试获取授权登录url

--- routes/weixin.js
+++ routes/weixin.js
@@ -111,6 +111,7 @@ router.post('/getAuthUrl', function (req, res) {
 //});
 
 router.post('/sendMessage', function (req, res) {
+    console.log(config);
     api.send({
         "touser": "1427945933704",
         "toparty": "",
