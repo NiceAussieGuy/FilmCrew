64869938a3be4925b4c1dd2d08dc200b8127d0bc
AmberGlasses <775837740@qq.com>
2015-05-07 13:50:05 +0800

    测试获取授权登录url

--- routes/weixin.js
+++ routes/weixin.js
@@ -60,6 +60,7 @@ router.post('/getJsConfig', function (req, res) {
     }
 
     var sign = sign('jsapi_ticket', url);
+    console.log(sign);
 
     var js_config = {
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
@@ -75,7 +76,6 @@ router.post('/getJsConfig', function (req, res) {
             'uploadImage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     };
 
-    console.log(sign);
     console.log('------------------------------');
     console.log(js_config);
     res.json(js_config);
